b8f9e90386673cd2d6869959ec827a49397172b5
Stephanie <stephcshea@gmail.com>
2018-06-18 06:19:57 -0400

    Completed uo to sec 3 kec 24

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -9,6 +9,15 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { Controller = "Movies", action= "ByReleaseDate" },
+            //    new { year = @"\d{4}", month = @"\d{2}" }
+            //);
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -1,6 +1,11 @@
-﻿using System.Web;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+
 
 namespace Vidly
 {
@@ -9,7 +14,9 @@ namespace Vidly
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
